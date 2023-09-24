de30c629a7317feebbeb749f128cdc10e5312d30
Sean Rawson <seancc8@gmail.com>
2023-09-24 14:56:47 -0400

    Add basic income operations

---
+++ code/back-end/src/main/java/met/cs673/team1/mapper/IncomeMapper.java
@@ -0,0 +1,36 @@
+package met.cs673.team1.mapper;
+
+import java.util.Optional;
+import met.cs673.team1.domain.dto.IncomeDto;
+import met.cs673.team1.domain.entity.Income;
+import met.cs673.team1.domain.entity.User;
+import met.cs673.team1.exception.UserNotFoundException;
+import met.cs673.team1.repository.UserRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public abstract class IncomeMapper {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Mapping(source = "username", target = "user")
+    public abstract Income incomeDtoToIncome(IncomeDto incomeDto);
+
+    public User mapUsernameToUser(String username) {
+        Optional<User> optUser = userRepository.findByUsername(username);
+        if (optUser.isEmpty()) {
+            throw new UserNotFoundException(String.format("User with username '%s' not found", username));
+        }
+        return optUser.get();
+    }
+
+    @Mapping(source = "user", target = "username")
+    public abstract IncomeDto incomeToIncomeDto(Income income);
+
+    public String mapUserToUsername(User user) {
+        return user.getUsername();
+    }
+}
