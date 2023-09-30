066a699df3b2f5a0e77160408f900875718378a1
Jisoo Kim <jkim136@bu.edu>
2023-09-30 17:28:37 -0400

    Changed structure in pennywise-ui

---
+++ code/pennywise-ui/src/components/sign-in/SetUp.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import {Avatar, Button, CssBaseline, TextField, FormControlLabel, Link, Grid, Typography, Container, Box} from '@mui/material';
+
+export default function setup() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+}
