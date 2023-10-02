fe4b038329699e44457a85459844f0c4b6b05f5f
Sean Rawson <seancc8@gmail.com>
2023-10-01 21:33:47 -0400

    Add @NotEmpty to addCategories argument

--- code/back-end/src/main/java/met/cs673/team1/controller/ExpenseCategoryController.java
+++ code/back-end/src/main/java/met/cs673/team1/controller/ExpenseCategoryController.java
@@ -1,13 +1,13 @@
 package met.cs673.team1.controller;
 
+import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
 import met.cs673.team1.domain.entity.ExpenseCategory;
 import met.cs673.team1.service.ExpenseCategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class ExpenseCategoryController {
@@ -19,7 +19,7 @@ public class ExpenseCategoryController {
     }
 
     @PostMapping(value = "/addCategories", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<ExpenseCategory>> addCategories(@RequestBody List<String> categories) {
+    public ResponseEntity<List<ExpenseCategory>> addCategories(@NotEmpty @RequestBody List<String> categories) {
         List<ExpenseCategory> categoriesWithIds = categoryService.saveAll(categories);
         return new ResponseEntity<>(categoriesWithIds, HttpStatus.CREATED);
     }
