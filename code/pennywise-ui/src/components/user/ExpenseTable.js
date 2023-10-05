ed84173b2f9282f0f9970a67104e5b3a00470ee6
mrivera6197 <mrivera6197@gmail.com>
2023-10-05 11:56:21 -0400

    expenseTable update

--- code/pennywise-ui/src/components/user/ExpenseTable.js
+++ code/pennywise-ui/src/components/user/ExpenseTable.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { DataGrid } from "@mui/x-data-grid"
 
 const columns = [
-    {field: "name", headerName: 'Expense', width: "320"}, 
-    {field: "category", headerName: "Expense Category", width: "320"}, 
-    {field: "amount", headerName: "Amount", width: "320"}, 
+    {field: "name", headerName: 'Expense', width: "250"}, 
+    {field: "category", headerName: "Expense Category", width: "250"}, 
+    {field: "amount", headerName: "Amount", width: "250"}, 
 ]
 
 
@@ -12,8 +12,8 @@ const columns = [
     
     return (
         <>
-        <div style={{height: 300, width: "90%", background:"#fff"}}>
-            <DataGrid
+        <div style={{height: 400, width: "100%", background:"#fff"}}>
+            <DataGrid sx={{fontSize:16}}
             getRowId={row => row.expenseId}
             rows={expenses}
             columns={columns}/>
