aafb9824bd5267dbddc92b64eb97e287723fe4e8
Mali Rivera <64143027+mrivera6197@users.noreply.github.com>
2023-10-06 16:28:20 -0400

    linked expense page

--- code/pennywise-ui/src/App.js
+++ code/pennywise-ui/src/App.js
@@ -1,7 +1,7 @@
 // import Home from "./components/home/Home"
 import Login from "./components/sign-in/Login"
 import SetUp from "./components/sign-in/SetUp"
-// import AddExpense from "./components/user/AddExpense"
+import AddExpense from "./components/user/AddExpense"
 // import AddIncome from "./components/user/AddIncome"
 import Dashboard from "./components/user/Dashboard"
 import NavBar from "./components/user/Navbar"
@@ -18,11 +18,11 @@ function App() {
    <NavBar/>
    
     <Routes>
-      {/* <Route path="/" element={<Home/>}></Route>
+      // <Route path="/home" element={<Home/>}></Route>
 
       
       <Route path="/addExpense" element={<AddExpense/>}></Route>
-      <Route path="/addIncome" element={<AddIncome/>}></Route> */}
+      // <Route path="/addIncome" element={<AddIncome/>}></Route>
       <Route path="/" element={<Login/>}></Route>
       <Route path="/dashboard" element={<Dashboard/>}></Route>
       <Route path="/setUp" element={<SetUp/>}></Route>
