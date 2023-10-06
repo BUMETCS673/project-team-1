4bc148a3ad99f6a659b6cb506f8b2183d26a17a0
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2023-10-05 21:46:55 -0400

    Update App.js

--- code/pennywise-ui/src/App.js
+++ code/pennywise-ui/src/App.js
@@ -1,6 +1,6 @@
 // import Home from "./components/home/Home"
 import Login from "./components/sign-in/Login"
-// import SetUp from "./components/sign-in/SetUp"
+import SetUp from "./components/sign-in/SetUp"
 // import AddExpense from "./components/user/AddExpense"
 // import AddIncome from "./components/user/AddIncome"
 import Dashboard from "./components/user/Dashboard"
@@ -20,11 +20,12 @@ function App() {
     <Routes>
       {/* <Route path="/" element={<Home/>}></Route>
 
-      <Route path="/setUp" element={<SetUp/>}></Route>
+      
       <Route path="/addExpense" element={<AddExpense/>}></Route>
       <Route path="/addIncome" element={<AddIncome/>}></Route> */}
       <Route path="/" element={<Login/>}></Route>
       <Route path="/dashboard" element={<Dashboard/>}></Route>
+      <Route path="/setUp" element={<SetUp/>}></Route>
     
 
     </Routes>
