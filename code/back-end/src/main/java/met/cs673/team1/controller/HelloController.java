693fd57480a9a8f144bd1d6f4dfd653b1ec0ed7b
mrivera6197 <mrivera6197@gmail.com>
2023-10-09 14:11:30 -0400

    helloController integration test

--- code/back-end/src/main/java/met/cs673/team1/controller/HelloController.java
+++ code/back-end/src/main/java/met/cs673/team1/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.fss.Springboot.tutorial.controller;
+package met.cs673.team1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
