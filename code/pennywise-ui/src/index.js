fa8442eb69538b415a00ba21206b960f954c1b72
Clyde Yeung <clydeyeung@clydes-mbp.lan>
2023-10-13 11:10:25 -0400

    Modify for PORT

--- code/pennywise-ui/src/index.js
+++ code/pennywise-ui/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-
+const port = process.env.PORT || 3000;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
@@ -15,5 +15,4 @@ root.render(
   </BrowserRouter>
 
 );
-
 
