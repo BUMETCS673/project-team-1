9d459db82ed03c059ab54181ebb71dce3bf0fb89
mrivera6197 <mrivera6197@gmail.com>
2023-10-13 21:22:23 -0400

    added expensePlot

--- code/pennywise-ui/src/components/user/ExpensePlot.js
+++ code/pennywise-ui/src/components/user/ExpensePlot.js
@@ -1,51 +1,52 @@
 import 'chart.js/auto';
-import { Pie } from "react-chartjs-2" 
+import {Line} from "react-chartjs-2" 
 import { Box, Typography } from "@mui/material"
 import ExpenseTable from './ExpenseTable';
 import { useState } from "react"
 import axios from "axios"
 
-const ExpenseChart = ( {expenses} ) => {
+const ExpensePlot = ( {expenses} ) => {
 
     // create array of unique expenseCategories from user expenses data
-    const getExpenseCategories = () => {
-        const expenseCategories = new Set(); 
+    const getExpenseDates = () => {
+        const expenseDates = new Set(); 
 
         expenses.map((expense) => {
-            expenseCategories.add(expense.category)
+            expenseDates.add(expense.date)
         })
-        return Array.from(expenseCategories); 
+        console.log(expenseDates)
+        return Array.from(expenseDates); 
 
     }
 
     // loop over expenses and calculate total amount spent for input expense category 
     // add key, value pair to input dict 
-    const makeCategory= (expenseCategory, dict) => {
+    const makeCategory= (expenseDate, dict) => {
       var amountSpent = 0
 
       expenses.map((expense) => {
-        if (expense.category === expenseCategory) {
+        if (expense.date === expenseDate) {
           amountSpent += expense.amount
-          dict.set(expense.category, amountSpent)
+          dict.set(expense.date, amountSpent)
         }
       })
     }
 
     // create dict, loop over expenseCategories and call makeCategory to add 
     // { expenseCategory : amountSpent } to dict 
-    const getCats = () => {
-      const expenseCategories = getExpenseCategories() 
+    const getDates = () => {
+      const expenseDates = getExpenseDates() 
       var dict = new Map();
 
-      expenseCategories.map((cat) => {
-        makeCategory(cat, dict)
+      expenseDates.map((date) => {
+        makeCategory(date, dict)
       })
 
       return dict
       
     }
 
-    const myMap = getCats()
+    const myMap = getDates()
     // create array from map and display pie chart of amountSpent per expenseCategory for user 
     const values = Array.from(myMap.values())
 
@@ -53,9 +54,9 @@ const ExpenseChart = ( {expenses} ) => {
 
     const data = {
         // set expenseCategories as labels
-        labels: getExpenseCategories().map((category) => category),
+        labels: getExpenseDates().map((date) => date),
         datasets: [{
-          label: 'My First Dataset',
+          label: 'Expenses',
           // set amountSpent per expenseCategory as data
           data: values.map((value) => value),
           backgroundColor: [
@@ -85,13 +86,13 @@ const ExpenseChart = ( {expenses} ) => {
 
         <>
 
-        <Box sx={{height: 450, width: "100%", display:"flex", flexDirection:"column", 
-        justifyContent:"center", alignItems:"center"}}>
-          <Typography sx={{fontSize:18, mb:1}}>
-            Expenses by Category
+        <Box sx={{height: 450, width: "90%", display:"flex", flexDirection:"column", 
+        justifyContent:"center", alignItems:"center", background: "#fff", p:5}}>
+          <Typography sx={{fontSize:18}}>
+            Expenses by Date
           </Typography>
 
-        <Pie data={data}/>
+        <Line data={data}/>
         </Box>
 
         </>
@@ -99,4 +100,4 @@ const ExpenseChart = ( {expenses} ) => {
     )
 }  
 
-export default ExpenseChart;
+export default ExpensePlot;
