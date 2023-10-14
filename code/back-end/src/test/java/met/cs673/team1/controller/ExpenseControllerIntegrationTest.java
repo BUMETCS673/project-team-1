0d89ee4f0e912bb19f3086b4c45f653020e473cb
mrivera6197 <mrivera6197@gmail.com>
2023-10-14 16:35:53 -0400

    updated incomeControllerIntegration test

--- code/back-end/src/test/java/met/cs673/team1/controller/ExpenseControllerIntegrationTest.java
+++ code/back-end/src/test/java/met/cs673/team1/controller/ExpenseControllerIntegrationTest.java
@@ -1,15 +1,98 @@
 package met.cs673.team1.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import met.cs673.team1.common.MonthYearFormatter;
+import met.cs673.team1.domain.dto.ExpenseDto;
+import met.cs673.team1.domain.entity.Role;
+import met.cs673.team1.domain.entity.User;
+import met.cs673.team1.service.ExpenseService;
+import met.cs673.team1.service.IncomeService;
+import met.cs673.team1.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(ExpenseController.class)
+@AutoConfigureMockMvc
 class ExpenseControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private MonthYearFormatter formatter;
+
+    @MockBean
+    private ExpenseService expenseService;
+
+    @MockBean
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
     @Test
-    public void test() {
+    void testFindAllByUserId() throws Exception {
+
+        Set<Role> roles =new HashSet<Role>();
+        Role role = new Role();
+        roles.add(role);
+
+        User user = new User();
+        user.setUsername("test");
+        user.setUserId(1);
+        user.setEmail("@.test");
+        user.setRoles(roles);
+        user.setLastName("mtest");
+        user.setFirstName("rtest");
+
+        LocalDate startDate = LocalDate.parse("2023-01-01");
+        LocalDate endDate = LocalDate.parse("2023-01-31");
+
+        // Create a list of mock ExpenseDto objects
+        List<ExpenseDto> mockExpenses = new ArrayList<>();
+
+        ExpenseDto expense1 = new ExpenseDto();
+        expense1.setExpenseId(1);
+        expense1.setUsername(user.getUsername());
+        expense1.setDate(startDate);
+        expense1.setCategory("food");
+        expense1.setAmount(20.00);
+        expense1.setName("potatoes");
+
+        mockExpenses.add(expense1);
+
 
         String hi = "hello";
         assert(hi.equals("hello"));
+
     }
 
 
--- code/back-end/src/test/java/met/cs673/team1/controller/IncomeControllerIntegrationTest.java
+++ code/back-end/src/test/java/met/cs673/team1/controller/IncomeControllerIntegrationTest.java
@@ -1,15 +1,66 @@
 package met.cs673.team1.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import met.cs673.team1.common.MonthYearFormatter;
+import met.cs673.team1.domain.dto.IncomeDto;
+import met.cs673.team1.service.IncomeService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@WebMvcTest(IncomeController.class)
+@AutoConfigureMockMvc
 class IncomeControllerIntegrationTest {
-    @Test
-    public void test() {
 
-        String hi = "hello";
-        assert(hi.equals("hello"));
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private MonthYearFormatter formatter;
+
+    @MockBean
+    private IncomeService incomeService;
+
+    @BeforeEach
+    void setUp() {
     }
 
+    @Test
+    void addUserIncome() throws Exception {
+        LocalDate date = LocalDate.of(2021, 10, 14);
+        IncomeDto incomeDto = new IncomeDto();
+        incomeDto.setUsername("test");
+        incomeDto.setAmount(1000.0);
+        incomeDto.setIncomeId(1);
+        incomeDto.setName("check");
+        incomeDto.setDate(date);
+
+        // Convert the IncomeDto to JSON
+        String jsonRequest = objectMapper.writeValueAsString(incomeDto);
+
+        // Perform a POST request to the "/addIncome" endpoint
+        mockMvc.perform(MockMvcRequestBuilders.post("/addIncome")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isCreated());
+    }
 }
