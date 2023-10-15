7e6daf3a2486d059876ab583a518b3193686366c
Clyde Yeung <33763916+clydeyeung@users.noreply.github.com>
2023-10-15 18:11:46 -0400

    Update Create_DB.sql add budget column

--- code/container/Create_DB.sql
+++ code/container/Create_DB.sql
@@ -8,7 +8,9 @@ CREATE TABLE User (
     #password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     first_name VARCHAR(255),
-    last_name VARCHAR(255));
+    last_name VARCHAR(255),
+    budget DECIMAL(10, 2));
+udget DECIMAL(10, 2));
 CREATE TABLE ExpenseCategory (
     category_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
