465bf65d06d0076f14399114f1e8c7be8cce33c6
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2023-10-15 16:57:56 -0400

    Update App.js

--- code/pennywise-ui/src/App.js
+++ code/pennywise-ui/src/App.js
@@ -5,7 +5,7 @@ import AddExpense from "./components/user/AddExpense"
 import AddIncome from "./components/user/AddIncome"
 import Dashboard from "./components/user/Dashboard"
 import NavBar from "./components/user/Navbar"
-
+import Signup from "./components/sign-in/Signin";
 import { Routes, Route} from "react-router-dom" 
 
 import './App.css';
@@ -23,7 +23,9 @@ function App() {
       <Route path="/addIncome" element={<AddIncome/>}></Route>
       <Route path="/" element={<Login/>}></Route>
       <Route path="/dashboard" element={<Dashboard/>}></Route>
-      <Route path="/setUp" element={<SetUp/>}></Route>
+      <Route path="/setUp" element={<SetUp/>}></Route>  
+      <Route path="/signup" element={<Signup/>}></Route>
+    
     
 
     </Routes>
