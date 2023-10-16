1566a645ad7c696edd4ea5a9159aee3b23f61aea
Clyde Yeung <33763916+clydeyeung@users.noreply.github.com>
2023-10-15 20:49:37 -0400

    Update Create_DB.sql put the password column back in

--- code/container/Create_DB.sql
+++ code/container/Create_DB.sql
@@ -5,7 +5,7 @@ USE pennywise_db;
 CREATE TABLE User (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    #password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
