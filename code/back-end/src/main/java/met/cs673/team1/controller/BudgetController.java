9d7bfdfbc58ed5324c451a6e749bfab0eef45311
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2023-10-17 19:33:11 -0400

    Update BudgetController.java -add CrossOrigin

--- code/back-end/src/main/java/met/cs673/team1/controller/BudgetController.java
+++ code/back-end/src/main/java/met/cs673/team1/controller/BudgetController.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class BudgetController {
 
